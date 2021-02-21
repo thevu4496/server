d96952ebad24301a414d88ea9e7c443b4ffccd40
thevu4496 <ts2a1996@gmail.com>
2021-02-21 19:39:57 +0700

    xxxx

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
-
+var io = require('socket.io')(server);
 var server = require('http').Server(app);
 var port = (process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 6969);
 
 server.listen(port, () => console.log('Server running in port ' + port));
-
+io.on('connection', function(socket) {
+  
+});
 app.get('/', (req, res) => {
   res.send("Home page. Server running okay.");
 })
